eae842846963b1ed3ef6600b380d1d0fadd3e94b
kdevidurgamala-wq <kdevidurgamala@gmail.com>
2025-11-25 10:48:58 +0530

    REVERSE THE NUMBER

---
+++ reverse of number .c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,p=1;
+	printf("enter n value");
+	scanf("%d",&n);
+	for(i=1,i<1,i++)
+	{
+		p=p*i
+	}
+	printf("\n reverse of the number =%d",s);
+	return 0;
+}
