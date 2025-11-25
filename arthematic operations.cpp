6687cde73e2e4d5b024d55dc049723df30cd45fc
kdevidurgamala-wq <kdevidurgamala@gmail.com>
2025-11-25 10:20:07 +0530

    ARTHEMATIC OPERATORS

---
+++ arthematic operations.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	scanf("%d %d",&a,&b);
+	int sum=a+b;
+	int difference=a-b;
+	int product=a*b;
+	int qoutient=a/b;
+	int remainder=a%b;
+	 printf("sum :%d\n",sum);
+	 printf("difference:%d\n",difference);
+	 printf("product:%d\n",product);
+	 printf("qoutient:%d\n",qoutient);
+	 printf("remainder:%d\n",remainder);
+	return 0;
+}
+
