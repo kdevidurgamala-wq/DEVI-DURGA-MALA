b5391192ec21933358202ce7528cffcc3578dd10
kdevidurgamala-wq <kdevidurgamala@gmail.com>
2026-04-29 22:04:44 +0530

    types of modules

---
+++ types of modules.py
@@ -0,0 +1,20 @@
+#types of mouldules-1.predefined,2.user defined
+ #predefined modules:it is already definrd by python.there are types of predefined modules
+#1.math:
+#2.random:
+#3.operating system:
+#4.system:
+#5.datetime:    
+#user defined:
+#examples-
+#maths:
+import math
+print(math.sqrt(100))
+#random:
+import random
+print(random.randint(1,20))
+#datetime:
+import datetime
+print(datetime.datetime.now())
+
+
