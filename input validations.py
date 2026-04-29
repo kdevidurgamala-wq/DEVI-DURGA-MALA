35c9ca52249b09026ea1937fca946624d227e296
kdevidurgamala-wq <kdevidurgamala@gmail.com>
2026-04-29 21:50:42 +0530

    input validations

---
+++ input validations.py
@@ -0,0 +1,24 @@
+age=input("enter age")
+if age.isdigit():
+   age=int(age)
+    print("valid age")
+else:
+    print("invalid age")
+    n=int(input("enter a number")
+if 1<n<10:
+    print("valid")
+else:
+    print("invalid")
+    p=input("enter password")
+if len(p)>=6:
+    print("too weak")
+    email=input("enter mail")
+else:
+    print("invalid password:")
+if'@'inemail and '.' in email:
+    print("valid mail")
+else:
+    print("invalid mail")
+         
+       
+         
