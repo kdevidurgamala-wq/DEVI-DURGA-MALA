a8920b3212bbfde9a61cec1e117db09907d48752
kdevidurgamala-wq <kdevidurgamala@gmail.com>
2026-04-29 22:05:26 +0530

    top-down design

---
+++ top-down design.py
@@ -0,0 +1,9 @@
+#top-down design:-the large prblm can be divided intlo sub prblms is called top-down design
+def add(a,b):
+    return a+b
+x=int(input("enter x value"))
+y=int(input("enter y value"))
+result=add(x,y)
+print(result)
+#recursion:- a  function call by itself is called recursion.every case have a base condition using this we can slove complex fuctions like finnaci,factorial,etc
+#syntax: if function-name()
