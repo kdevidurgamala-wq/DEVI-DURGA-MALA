3a4a90fcacaa707ac68727fa8259ce031d16c950
kdevidurgamala-wq <kdevidurgamala@gmail.com>
2026-04-29 22:00:48 +0530

    spy number

---
+++ spy number.py
@@ -0,0 +1,13 @@
+n=int(input("enter a number:"))
+sum_digits=0
+product_digits=1
+temp=n
+while temp>0:
+    sum_digits +=digit
+    product_digit*=digit
+    temp= temp//10
+if sum_digits==product_digits:
+       print("spy number")
+else:
+    print("not a spy number")
+ 
